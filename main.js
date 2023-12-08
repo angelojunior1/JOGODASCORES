c93197ff3f6e68efd48cfac8737ab41d3d03ca42
Angelo Marcos Siqueira <88817332+angelojunior1@users.noreply.github.com>
2023-12-08 14:56:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ const opcaoCorDesafio = document.querySelectorAll(".opcaoCor");
 const respostaTela = document.querySelector("#correto");
 const corDisplay = document.querySelector("h1");
 const button = document.querySelector("button");
+const todasCoresVetor = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+// Selecionar 10 cores aleatórias
 
 let cores = [];
-let tentativas = 3; // Defina o número desejado de tentativas
+let tentativas = 3; // Num Tentativas
+let corReset = 'black';
+let corEscolhida; // Cor correta
+
+function pintarBody(cor) {
+    document.body.style.backgroundColor = cor;
+}
 
 GerarCorAleatória();
 cores_atribuidas_rodada();
+corEscolhida = obterCorAleatoria(); // Inicialize corEscolhida
 verificaCor();
 atualizarTentativas();
 
@@ -58,28 +68,37 @@ function resetar() {
     GerarCorAleatória();
     opcaoCorDesafio.forEach((opcaoCor) => opcaoCor.classList.remove("hide"));
     cores_atribuidas_rodada();
-    verificaCor();
     corEscolhida = obterCorAleatoria();
     respostaTela.textContent = "";
     atualizarTentativas();
+    pintarBody(corReset); 
 }
 
-function pintarBody(cor) {
-    document.body.style.backgroundColor = cor;
+function rgbToColorName(rgb) {
+    const index = todasCoresVetor.findIndex((cor) => cor === rgb);
+    return todasCoresVetor[index];
 }
 
 function encerrarJogoVitoria() {
-    alert(`Parabéns! Você acertou a cor e venceu o jogo! O background agora é da cor escolhida.`);
-    resetar();
+    const corNome = rgbToColorName(corEscolhida);
+
+    alert(`Parabéns! Você acertou a cor e venceu o jogo! O background agora é da cor escolhida: ${corNome}`);
+    resetar(corReset);
+
+    pintarBody(corReset); // resetar background para o preto
 }
 
 function encerrarJogoDerrota() {
-    alert(`Suas tentativas acabaram. Você perdeu! A cor escolhida era: ${corEscolhida}`);
-    resetar();
+    const corNome = rgbToColorName(corEscolhida);
+
+    alert(`Suas tentativas acabaram. Você perdeu! A cor escolhida era: ${corNome}`);
+    resetar(corReset);
 }
 
 function atualizarTentativas() {
-    // Adicione aqui o código para atualizar a interface do usuário com o número de tentativas restantes
-}
+    // Supondo que você tenha um elemento HTML com o id "tentativasRestantes" para exibir as tentativas restantes
+    const tentativasRestantesElement = document.getElementById("tentativasRestantes");
 
-let corEscolhida = obterCorAleatoria();
+    // Atualizar o conteúdo do elemento com o número de tentativas restantes
+    tentativasRestantesElement.textContent = `Tentativas Restantes: ${tentativas}`;
+}
